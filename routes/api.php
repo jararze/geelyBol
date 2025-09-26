5a65b9b494fb01a69b268dca53991a211536e662
Jorge <jararze@gmail.com>
2025-09-26 10:51:36 -0400

    cambios front

--- routes/api.php
+++ routes/api.php
@@ -51,18 +51,18 @@ Route::get('/export/excel-data', function() {
 Route::get('/export/purchased-data', function() {
     $submissions = \App\Models\PurchasedVehicleForm::orderBy('created_at', 'desc')->get();
 
-    // Encabezados con todos los campos
-    $output = "ID\tNombre\tApellido\tSegundo Apellido\tGénero\tNacionalidad\tDocumento ID\tFecha Nacimiento\tTeléfono\tEmail\tQuiere Promociones\tPromo WhatsApp\tPromo Email\tPromo SMS\tSin Promociones\tCiudad\tBarrio\tDirección Completa\tEstado Civil\tTiene Hijos\tNúmero de Hijos\tCampo de Trabajo\tNombre Asesor\tVehículo Comprado\tCaracterística Atractiva\tHobbies\tNivel Educación\tConductor Principal\tFecha Creación\tFecha Actualización\n";
+    // Encabezados sin espacios ni caracteres especiales
+    $output = "ID\tNombre\tApellido\tSegundoApellido\tGenero\tNacionalidad\tDocumentoID\tFechaNacimiento\tTelefono\tEmail\tQuierePromociones\tPromoWhatsApp\tPromoEmail\tPromoSMS\tSinPromociones\tCiudad\tBarrio\tDireccionCompleta\tEstadoCivil\tTieneHijos\tNumeroDeHijos\tCampoDeTrabajo\tNombreAsesor\tVehiculoComprado\tCaracteristicaAtractiva\tHobbies\tNivelEducacion\tConductorPrincipal\tFechaCreacion\tFechaActualizacion\n";
 
     foreach($submissions as $sub) {
-        // Procesar hobbies (JSON)
+        // Procesar hobbies como string
         $hobbies = '';
         if ($sub->hobbies) {
             if (is_string($sub->hobbies)) {
                 $hobbiesArray = json_decode($sub->hobbies, true);
-                $hobbies = is_array($hobbiesArray) ? implode(', ', $hobbiesArray) : $sub->hobbies;
+                $hobbies = is_array($hobbiesArray) ? implode('; ', $hobbiesArray) : $sub->hobbies;
             } else if (is_array($sub->hobbies)) {
-                $hobbies = implode(', ', $sub->hobbies);
+                $hobbies = implode('; ', $sub->hobbies);
             }
         }
 
@@ -74,33 +74,33 @@ Route::get('/export/purchased-data', function() {
                 $sub->gender ?? '',
                 $sub->nationality ?? '',
                 $sub->id_document ?? '',
-                $sub->birth_date ?? '',
+                $sub->birth_date ? date('d/m/Y', strtotime($sub->birth_date)) : '',
                 $sub->mobile_phone ?? '',
                 $sub->email ?? '',
-                $sub->wants_promotions ? 'Sí' : 'No',
-                $sub->promo_whatsapp ? 'Sí' : 'No',
-                $sub->promo_email ? 'Sí' : 'No',
-                $sub->promo_sms ? 'Sí' : 'No',
-                $sub->no_promotions ? 'Sí' : 'No',
+                $sub->wants_promotions ? 'Si' : 'No',
+                $sub->promo_whatsapp ? 'Si' : 'No',
+                $sub->promo_email ? 'Si' : 'No',
+                $sub->promo_sms ? 'Si' : 'No',
+                $sub->no_promotions ? 'Si' : 'No',
                 $sub->city ?? '',
                 $sub->neighborhood ?? '',
                 str_replace(["\n", "\r", "\t"], ' ', $sub->full_address ?? ''),
                 $sub->marital_status ?? '',
-                $sub->has_children ? 'Sí' : 'No',
+                $sub->has_children ? 'Si' : 'No',
                 $sub->number_of_children ?? '',
                 $sub->work_field ?? '',
                 $sub->sales_advisor_name ?? '',
                 $sub->purchased_vehicle ?? '',
                 str_replace(["\n", "\r", "\t"], ' ', $sub->vehicle_attractive_feature ?? ''),
-                $hobbies,
+                str_replace(["\n", "\r", "\t"], ' ', $hobbies),
                 $sub->education_level ?? '',
                 $sub->main_driver ?? '',
-                $sub->created_at ? $sub->created_at->format('d/m/Y H:i:s') : '',
-                $sub->updated_at ? $sub->updated_at->format('d/m/Y H:i:s') : ''
+                $sub->created_at ? $sub->created_at->format('d/m/Y H:i') : '',
+                $sub->updated_at ? $sub->updated_at->format('d/m/Y H:i') : ''
             ]) . "\n";
     }
 
     return response($output, 200, [
-        'Content-Type' => 'text/plain; charset=utf-8',
+        'Content-Type' => 'text/plain; charset=utf-8'
     ]);
 });
