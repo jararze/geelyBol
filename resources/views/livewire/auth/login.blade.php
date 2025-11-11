b7c3a650def400d19d39970f1d82da60e4ad3a3a
Jorge <jararze@gmail.com>
2025-11-11 14:37:44 -0400

    cambios front

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -119,9 +119,9 @@ new #[Layout('components.layouts.auth')] class extends Component {
     </form>
 
     @if (Route::has('register'))
-        <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-            <span>{{ __('Don\'t have an account?') }}</span>
-            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
-        </div>
+{{--        <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">--}}
+{{--            <span>{{ __('Don\'t have an account?') }}</span>--}}
+{{--            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>--}}
+{{--        </div>--}}
     @endif
 </div>
