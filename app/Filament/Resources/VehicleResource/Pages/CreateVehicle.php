dff63f19a8d53b3807b46a634eabdd493511aea5
Jorge <jararze@gmail.com>
2026-05-04 23:02:04 -0400

    feat: pre-fill page_blocks al crear vehiculo

--- app/Filament/Resources/VehicleResource/Pages/CreateVehicle.php
+++ app/Filament/Resources/VehicleResource/Pages/CreateVehicle.php
@@ -3,9 +3,19 @@
 namespace App\Filament\Resources\VehicleResource\Pages;
 
 use App\Filament\Resources\VehicleResource;
+use App\Services\VehicleBlocksDefaults;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateVehicle extends CreateRecord
 {
     protected static string $resource = VehicleResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (empty($data['page_blocks'])) {
+            $data['page_blocks'] = VehicleBlocksDefaults::generate();
+        }
+
+        return $data;
+    }
 }
